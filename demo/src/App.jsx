053369b9a117ae93a0c521107089982524267645
ritiksawarkar <ritiksawarkar027@gmail.com>
2026-02-06 14:18:15 +0530

    Resolve merge conflict in App.jsx

--- demo/src/App.jsx
+++ demo/src/App.jsx
@@ -1,10 +1,15 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import About from "./pages/About";
 import Contact from "./pages/Contact";
 
 function App() {
   return (
-    <div>
-      <Contact />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
   );
 }
 
